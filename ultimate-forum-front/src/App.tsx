2ed1e4bb8b5c5cbb00272dd205b08f501796df79
AXCWG <xiey0@qq.com>
2026-05-08 08:58:26 +0800

    checkpoint

--- ultimate-forum-front/src/App.tsx
+++ ultimate-forum-front/src/App.tsx
@@ -1,5 +1,5 @@
-import {Component, createResource, Suspense} from 'solid-js';
-import {UltimateForum} from "./SIngleton";
+import {Component, createEffect, createResource, createSignal, on, Show, Suspense} from 'solid-js';
+import {CutByEllipsis, UltimateForum} from "./SIngleton";
 import {A} from "@solidjs/router";
 import {For} from "solid-js"
 import Enumerable from "linq";
@@ -10,26 +10,39 @@ const App: Component = () => {
 			credentials: "include"
 		})).data
 	})
-
+	const m = window.matchMedia("(width >= 48rem)")
+	const [signal_evf87, set_signal_evf87] = createSignal<boolean>(m.matches)
+	m.addEventListener('change', ()=>{
+		set_signal_evf87(m.matches)
+	});
+	createEffect(on(signal_evf87, ()=>{
+		console.log(signal_evf87())
+	}))
+	const hiddenCell = "md:table-cell hidden"
   return (
 	  <Suspense>
 		  <>
 			  <div class={"border-b-1 flex justify-between border-t-1 p-2  border-gray-300 px-5"}>
 				  <div class={"font-bold"}>Ultimate Forum</div>
+
 				  <div class={"flex gap-3"}>
 					  <A href={"/login"}>Login</A>
 					  <A href={"/register"}>Register</A>
 				  </div>
 			  </div>
-			  <div class={"mx-50 mt-10"}>
-				  <div class={"text-6xl font-light"}>Ultimate Forum</div>
-				  <table class={"table table-md"}>
+			  <div class={"lg:mx-50 mx-10 mt-10"}>
+				  <div class={"mb-5"}>
+					  <div class={"text-6xl font-light"}>Ultimate Forum</div>
+					  <div class={"text-base-content/60"}>Good forum software. </div>
+				  </div>
+
+				  <table class={"table table-sm border border-base-content/5 table-fixed"}>
 					  <thead>
 					  	<tr>
 							<td>Board Name</td>
-							<td>Board Desc.</td>
+
 							<td>Newest Activity</td>
-							<td>Post-Reply count</td>
+							<td class={hiddenCell}>Post-Reply count</td>
 							<td>Operator</td>
 						</tr>
 					  </thead>
@@ -38,13 +51,16 @@ const App: Component = () => {
 						  {
 							  x=>{
 								  return <tr>
-									  <td>{x.name}</td>
-									  <td>{x.description}</td>
+									  <td>
+										  {x.name}<br/>
+										  <span class={"text-base-content/60"}>{x.description}</span>
+									  </td>
 									  <td>{x.posts && x.posts.length !== 0 ? (()=> {
 											  let t = Enumerable.from(x.posts).orderByDescending(p => p.createdAt).first();
-											  return <A href={`/post/${t.id}`}>{t.title}</A>
+											  return <Show when={signal_evf87()} fallback={<A href={`/post/${t.id}`}>{CutByEllipsis(t.title)}</A>}><A href={`/post/${t.id}`}>{t.title}</A></Show>
 									  })() : <>No activity.</>}</td>
-									  <td>{x.posts ? x.posts.length + Enumerable.from(x.posts).sum(p=>p.replies ? p.replies.length : 0) : null}</td>
+									  <td class={hiddenCell}>{x.posts ? x.posts.length + Enumerable.from(x.posts).sum(p=>p.replies ? p.replies.length : 0) : null}</td>
+									  <td>{x.op?.username}</td>
 								  </tr>
 							  }
 						  }
--- ultimate-forum-front/src/SIngleton.ts
+++ ultimate-forum-front/src/SIngleton.ts
@@ -1,3 +1,4 @@
 import * as api from "./client/sdk.gen"
 const UltimateForum = api;
-export {UltimateForum}
+const CutByEllipsis = (source: string|null | undefined) : string | null=> source ?  source.length >= 10 ? source?.substring(0, 11) + "..." : source??null : null
+export {UltimateForum, CutByEllipsis}
